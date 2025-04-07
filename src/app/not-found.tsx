46f41add380b68de292a82e1dc24eefb6b70cdfc
KyawMinHtet100 <kyawminhtet1792000@gmail.com>
2025-04-08 02:58:05 +0700

    project setup commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h3 className="text-2xl font-semibold">Page Not Found</h3>
+    </div>
+  );
+};
+
+export default PageNotFound;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full max-w-5xl items-center justify-between lg:flex">
+        <h1 className="text-2xl font-semibold">Hello there</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
